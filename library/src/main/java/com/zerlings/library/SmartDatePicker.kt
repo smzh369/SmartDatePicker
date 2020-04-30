5dccb3a34bbbf3250784f0eb34e86ee064b1a0e6
Zerlings <smzh369@126.com>
2020-05-01 02:05:57 +0800

    1.点击事件。2.日期选择监听。

--- library/src/main/java/com/zerlings/library/SmartDatePicker.kt
+++ library/src/main/java/com/zerlings/library/SmartDatePicker.kt
@@ -1,10 +1,13 @@
 package com.zerlings.library
 
+import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import android.content.Context
 import android.support.v7.widget.AppCompatTextView
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.widget.DatePicker
+import java.text.SimpleDateFormat
 import java.util.*
 
 class SmartDatePicker @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = android.R.attr.textViewStyle) : AppCompatTextView(context, attrs, defStyleAttr) {
@@ -17,9 +20,11 @@ class SmartDatePicker @JvmOverloads constructor(context: Context, attrs: Attribu
 
     private var mDay = calendar.get(Calendar.DATE)
 
-    private var mTimeStamp = System.currentTimeMillis()/1000
+    private var mTimeStamp = System.currentTimeMillis() / 1000
 
     private var mDateStr = DateUtils.timeStamp2Date(mTimeStamp.toString())
+
+    private val sdf: SimpleDateFormat
 
     private val format: String
 
@@ -32,14 +37,31 @@ class SmartDatePicker @JvmOverloads constructor(context: Context, attrs: Attribu
     init {
         val typedArray = context.obtainStyledAttributes(attrs, R.styleable.SmartDatePicker)
         format = typedArray.getString(R.styleable.SmartDatePicker_format) ?: "yyyy-MM-dd"
+        sdf = SimpleDateFormat(format)
         style = typedArray.getInt(R.styleable.SmartDatePicker_style, 3)
         dialog = DatePickerDialog(context, style, DatePickerDialog.OnDateSetListener{ datePicker: DatePicker, year: Int, month: Int, day: Int ->
+            mYear = year
+            mMonth = month + 1
+            mDay = day
             calendar.set(year, Calendar.YEAR)
             calendar.set(month, Calendar.MONTH)
             calendar.set(day, Calendar.DATE)
-            mDateStr = "$year"
+            mTimeStamp = calendar.timeInMillis / 1000
+            mDateStr = sdf.format(calendar.time)
+            onDatePickListener?.invoke(datePicker, mYear, mMonth, mDay, mTimeStamp, mDateStr)
         }, mYear, mMonth-1, mDay)
         typedArray.recycle()
     }
 
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        if (isEnabled && event.action == MotionEvent.ACTION_UP){
+            if (!dialog.isShowing){
+                dialog.show()
+            }else{
+                dialog.dismiss()
+            }
+        }
+        return super.onTouchEvent(event)
+    }
 }
