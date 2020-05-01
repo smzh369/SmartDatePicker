0a3e256aebe5b8b78a1d3e7041db36cd80740a1e
Zerlings <smzh369@126.com>
2020-05-01 22:04:53 +0800

    1.完善demo。2.增加最小最大日期。3.可选隐藏日。4.完善各项属性设置和点击监听。

--- app/src/main/java/com/zerlings/smartdatepicker/MainActivity.kt
+++ app/src/main/java/com/zerlings/smartdatepicker/MainActivity.kt
@@ -2,11 +2,20 @@ package com.zerlings.smartdatepicker
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        date_picker.setOnDatePickListener { datePicker, year, month, day, timeStamp, dateStr ->
+            Log.i("SmartDatePicker", "year : $year")
+            Log.i("SmartDatePicker", "month : $month")
+            Log.i("SmartDatePicker", "day : $day")
+            Log.i("SmartDatePicker", "dateStr : $dateStr")
+            Log.i("SmartDatePicker", "timeStamp : $timeStamp")
+        }
     }
 }
--- library/src/main/java/com/zerlings/library/SmartDatePicker.kt
+++ library/src/main/java/com/zerlings/library/SmartDatePicker.kt
@@ -5,14 +5,22 @@ import android.app.DatePickerDialog
 import android.content.Context
 import android.support.v7.widget.AppCompatTextView
 import android.util.AttributeSet
+import android.util.Log
 import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
 import android.widget.DatePicker
 import java.text.SimpleDateFormat
 import java.util.*
 
 class SmartDatePicker @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = android.R.attr.textViewStyle) : AppCompatTextView(context, attrs, defStyleAttr) {
 
-    private val calendar = Calendar.getInstance()
+    private val calendar = Calendar.getInstance().apply {
+        set(Calendar.HOUR_OF_DAY, 0)
+        set(Calendar.MINUTE, 0)
+        set(Calendar.SECOND, 0)
+        set(Calendar.MILLISECOND, 0)
+    }
 
     private var mYear = calendar.get(Calendar.YEAR)
 
@@ -20,15 +28,21 @@ class SmartDatePicker @JvmOverloads constructor(context: Context, attrs: Attribu
 
     private var mDay = calendar.get(Calendar.DATE)
 
-    private var mTimeStamp = System.currentTimeMillis() / 1000
+    private var mTimeStamp = calendar.timeInMillis
 
-    private var mDateStr = DateUtils.timeStamp2Date(mTimeStamp.toString())
+    private var mDateStr = ""
 
     private val sdf: SimpleDateFormat
 
     private val format: String
 
+    private val minDate: String?
+
+    private val maxDate: String?
+
     private val style: Int
+
+    private val timeMillis: Boolean
 
     private val dialog: DatePickerDialog
 
@@ -37,20 +51,37 @@ class SmartDatePicker @JvmOverloads constructor(context: Context, attrs: Attribu
     init {
         val typedArray = context.obtainStyledAttributes(attrs, R.styleable.SmartDatePicker)
         format = typedArray.getString(R.styleable.SmartDatePicker_format) ?: "yyyy-MM-dd"
-        sdf = SimpleDateFormat(format)
+        minDate = typedArray.getString(R.styleable.SmartDatePicker_minDate)
+        maxDate = typedArray.getString(R.styleable.SmartDatePicker_maxDate)
         style = typedArray.getInt(R.styleable.SmartDatePicker_style, 3)
+        timeMillis = typedArray.getBoolean(R.styleable.SmartDatePicker_timeMills, false)
+        typedArray.recycle()
+        sdf = SimpleDateFormat(format)
         dialog = DatePickerDialog(context, style, DatePickerDialog.OnDateSetListener{ datePicker: DatePicker, year: Int, month: Int, day: Int ->
             mYear = year
             mMonth = month + 1
             mDay = day
-            calendar.set(year, Calendar.YEAR)
-            calendar.set(month, Calendar.MONTH)
-            calendar.set(day, Calendar.DATE)
-            mTimeStamp = calendar.timeInMillis / 1000
+            calendar.set(Calendar.YEAR, year)
+            calendar.set(Calendar.MONTH, month)
+            calendar.set(Calendar.DATE, day)
+            mTimeStamp = if (timeMillis) calendar.timeInMillis else calendar.timeInMillis / 1000
             mDateStr = sdf.format(calendar.time)
+            text = mDateStr
             onDatePickListener?.invoke(datePicker, mYear, mMonth, mDay, mTimeStamp, mDateStr)
         }, mYear, mMonth-1, mDay)
-        typedArray.recycle()
+        if (!format.contains("dd")){
+            ((dialog.datePicker.getChildAt(0) as ViewGroup).getChildAt(0) as ViewGroup).getChildAt(2).visibility = View.GONE
+        }
+        minDate?.apply { sdf.parse(this)?.let { dialog.datePicker.minDate = it.time } }
+        maxDate?.apply { sdf.parse(this)?.let { dialog.datePicker.maxDate = it.time } }
+        mDateStr = sdf.format(mTimeStamp)
+        text = mDateStr
+        isClickable = true
+        Log.i("SmartDatePicker", "year : $mYear")
+        Log.i("SmartDatePicker", "month : $mMonth")
+        Log.i("SmartDatePicker", "day : $mDay")
+        Log.i("SmartDatePicker", "dateStr : $mDateStr")
+        Log.i("SmartDatePicker", "timeStamp : $mTimeStamp")
     }
 
     @SuppressLint("ClickableViewAccessibility")
@@ -63,5 +94,20 @@ class SmartDatePicker @JvmOverloads constructor(context: Context, attrs: Attribu
             }
         }
         return super.onTouchEvent(event)
+    }
+
+    fun setStartDate(year: Int, month: Int, day: Int){
+        mYear = year
+        mMonth = month
+        mDay = day
+        calendar.set(year, month-1, day)
+        mTimeStamp = if (timeMillis) calendar.timeInMillis else calendar.timeInMillis / 1000
+        mDateStr = sdf.format(calendar.time)
+        text = mDateStr
+        dialog.datePicker.updateDate(year, month, day)
+    }
+
+    fun setOnDatePickListener(listener: (DatePicker, Int, Int, Int, Long, String) -> Unit){
+        onDatePickListener = listener
     }
 }
